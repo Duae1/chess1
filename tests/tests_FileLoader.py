0080f041c11c44ca7d95c4b533094e67a2f25465
AimanGK0 <119605296+Aimangk@users.noreply.github.com>
2026-03-02 20:52:42 +0100

    Implement test for FileLoader CSV functionality
    Add a test for the FileLoader class to validate CSV reading.

--- tests/tests_FileLoader.py
+++ tests/tests_FileLoader.py
@@ -1,1 +1,22 @@
+import sys 
+import os 
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from model.file_loader import FileLoader
+
+def test_file_loader():
+  data = FileLoader("lichess.csv").read_csv()
+
+  assert data is not None,
+  assert isinstance(data, list),
+  if len(data)>0:
+    print("Fichier chargé ")
+
+  nodata = FileLoader("").read_csv()
+
+  assert result == ""
+  print("Test fichier manquant OK")
+
+if __name__ == "__main__":
+    test_file_loader()
