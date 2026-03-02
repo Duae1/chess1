1e5217c56fec020bd8b1fa07c932db9b25733d2e
AimanGK0 <119605296+Aimangk@users.noreply.github.com>
2026-03-02 20:54:49 +0100

    Refactor assertions in tests_FileLoader.py
    Update assertions in test_file_loader for clarity.

--- tests/tests_FileLoader.py
+++ tests/tests_FileLoader.py
@@ -8,14 +8,14 @@ from model.file_loader import FileLoader
 def test_file_loader():
   data = FileLoader("lichess.csv").read_csv()
 
-  assert data is not None,
-  assert isinstance(data, list),
+  assert data is not None, "Pas de retour None"
+  assert isinstance(data, list), "Il faut un retour liste"
   if len(data)>0:
     print("Fichier chargé ")
 
   nodata = FileLoader("").read_csv()
 
-  assert result == ""
+  assert nodata == []
   print("Test fichier manquant OK")
 
 if __name__ == "__main__":
