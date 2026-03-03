66dc450777587353f2454d393e9d3e2c93963dac
AimanGK0 <119605296+Aimangk@users.noreply.github.com>
2026-03-03 14:45:12 +0100

    Change file name to 'Lichess.csv' in main.py
    Updated the file name in the FileLoader to 'Lichess.csv'.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from model.fen import Fen
 from view.view import ChessBoard
 
 def main():
-    loader = FileLoader("lichess.csv")
+    loader = FileLoader("Lichess.csv")
     all_puzzles = loader.read_csv()
     
     if all_puzzles:
@@ -29,6 +29,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
